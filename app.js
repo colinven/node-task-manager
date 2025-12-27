0df5fe02664f544460ee2693b24cdc8ac0ca6977
ColinVenancio <232087417+colinven@users.noreply.github.com>
2025-12-27 13:18:47 -0500

    Added delete, update, mark-in-progress, mark-done commands

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function writeTasks(tasks) {
     fs.writeFileSync(tasksFilePath, JSON.stringify(tasks, null, 2), 'utf8');
 };
 
-// GET NEXT AVAILABLE ID #, IF A TASK WAS PREVIOUSLY DELETED, IT'S ID # WILL BE REUSED
+//need to fix this function, iterations start at lowest number, meaning smaller nums will not be reused
 function getNextId() {
     const tasks = readTasks();
     const allCurrentIds = tasks.map((task) => task.id);
@@ -48,9 +48,8 @@ function getNextId() {
         }
     }
     return 1;
-}
+};
 
-// CREATE NEW TASK OBJ, WRITE TO TASKS.JSON
 function addTask(description) {
     const tasks = readTasks();
     const newTask = {
@@ -59,6 +58,10 @@ function addTask(description) {
         status: 'todo',
         createdAt: new Date(),
         updatedAt: null
+    }
+    if (description === ''){
+        console.log("Could not add task: you must provide a description.");
+        return;
     }
     try {
         tasks.push(newTask);
@@ -69,21 +72,116 @@ function addTask(description) {
     }
 };
 
+function listTasks(taskStatus) {
+    const tasks = readTasks();
+    const validStatuses = ['todo', 'in-progress', 'done', ''];
+    if (!validStatuses.includes(taskStatus)){
+        console.log("Could not list tasks: you must provide a valid status.\nValid Statuses: (todo, in-progress, done)");
+        return;
+    }
+    if (taskStatus === '') {
+        tasks.forEach((task) => console.log(`------\nDescription: ${task.description}\nStatus: ${task.status}\nTask ID: ${task.id}\n`));
+    } else {
+        const selectedTasks = tasks.filter((task) => task.status === taskStatus);
+        selectedTasks.forEach((task) => {
+            console.log(`------\nDescription: ${task.description}\nStatus: ${task.status}\nTask ID: ${task.id}\n`);
+        }) 
+    }
+};
+
+function deleteTask(id){
+    const tasks = readTasks();
+
+    if (typeof id !== "number" || Number.isNaN(id)){
+        console.log('Could not delete task: you must provide a valid task id number.');
+        return;
+    };
+
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+
+    if (taskIndex === -1) {
+        console.log(`Could not delete task: task with id ${id} was not found.`);
+        return;
+    }
+    const deletedTask = tasks[taskIndex];
+    tasks.splice(taskIndex, 1);
+    writeTasks(tasks);
+    console.log(`** Task ${deletedTask.id} Successfully Deleted **`);
+}
+
+function updateTask(id, newDesc) {
+    const tasks = readTasks();
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+    if (taskIndex === -1) {
+        console.log(`Could not update task: could not find task with id ${id}.`);
+        return;
+    } else if (newDesc === ''){
+        console.log('Could not update task: please provide a valid description.');
+        return;
+    } else {
+        const updatedTask = tasks[taskIndex]
+        updatedTask.description = newDesc;
+        updatedTask.updatedAt = new Date();
+        writeTasks(tasks);
+        console.log(`** Successfully Updated Task ID ${id}**`)
+    }
+};
+
+function changeStatus(status, id) {
+    const tasks = readTasks();
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+    if (id === '') {
+        console.log('Could not update task: please provide a valid id.');
+        return;
+    };
+    if (taskIndex === -1) {
+        console.log(`Could not update task: could not find task with id ${id}.`);
+        return;
+    };
+    const updatedTask = tasks[taskIndex];
+    if (status === "mark-in-progress") {
+        updatedTask.status = "in-progress";
+    }
+    if (status === "mark-done") {
+        updatedTask.status = "done";
+    }
+    updatedTask.updatedAt = new Date();
+    console.log(`** Successfully Updated Task ID ${id}**`);
+    writeTasks(tasks);
+}
+
+
 // COMMAND LINE LOGIC
 const args = process.argv.slice(2);
-const command = args[0];
+const command = args[0].toLowerCase();
 const commandArgs = args.slice(1);
+const argsFormatted = commandArgs.join(" ").trim();
 
 switch (command) {
     case "add":
-        if (!commandArgs || commandArgs.length === 0){
-            console.log(`Could not add task: you must provide a description.`);
-            break;
-        }
-
-        addTask(commandArgs.join(" "));
+        addTask(argsFormatted);
         break;
-    
+
+    case "list":
+        listTasks(argsFormatted);
+        break;
+        
+    case "delete":
+        deleteTask(+argsFormatted);
+        break;
+
+    case "update":
+        updateTask(+commandArgs[0], commandArgs.slice(1).join(" "));
+        break;
+
+    case "mark-in-progress":
+        changeStatus("mark-in-progress", +commandArgs[0]);
+        break;
+
+    case "mark-done":
+        changeStatus("mark-done", +commandArgs[0]);
+        break;
+
     default:
         console.log('Unknown command');
 }
